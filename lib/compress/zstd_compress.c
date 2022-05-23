4ca873bfe1f4f42a1b55bf6cfea12d3e8edd5944
Elliot Gorokhovsky <embg@fb.com>
2022-05-23 17:29:28 -0400

    fix CI

--- lib/compress/zstd_compress.c
+++ lib/compress/zstd_compress.c
@@ -2131,9 +2131,18 @@ static size_t ZSTD_resetCCtx_byCopyingCDict(ZSTD_CCtx* cctx,
                                                             : 0;
         size_t const hSize =  (size_t)1 << cdict_cParams->hashLog;
 
-        ZSTD_memcpy(cctx->blockState.matchState.hashTable,
+        /* ZSTD_memcpy(cctx->blockState.matchState.hashTable,
                cdict->matchState.hashTable,
-               hSize * sizeof(U32));
+               hSize * sizeof(U32)); */
+
+        // TODO make this "if fast" only
+        {
+            size_t i;
+            for (i=0; i < hSize; i++) {
+                cdict->matchState.hashTable[i] = cctx->blockState.matchState.hashTable[i] >> 8; // TODO make this reference macro
+            }
+        }
+
         /* Do not copy cdict's chainTable if cctx has parameters such that it would not use chainTable */
         if (ZSTD_allocateChainTable(cctx->appliedParams.cParams.strategy, cctx->appliedParams.useRowMatchFinder, 0 /* forDDSDict */)) {
             ZSTD_memcpy(cctx->blockState.matchState.chainTable,
